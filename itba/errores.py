0a3d689fdc79189a263e9ef58d48bc2c36794db5
lichaa824 <lisandrorubi@gmail.com>
2022-07-17 21:35:15 -0300

    commit

--- itba/errores.py
+++ itba/errores.py
@@ -23,3 +23,36 @@ class Buscador():
                 if x['monto']>x['cupoDiarioRestante']:
                     x.update(razon= "El retiro de  $" + str(x['monto']) + " exede el cupo diario restante de $" + str(x['cupoDiarioRestante']))
                     x['tipo'] ="RETIRO EFECTIVO CAJERO AUTOMATICO"
+
+
+            elif x['tipo']== "ALTA_TARJETA_CREDITO":
+                if x['totalTarjetasDeCreditoActualmente'] >= 5:
+                    x.update(razon= "Actualmente tiene el maximo de tarjetas de credito ("+str(x['totalTarjetasDeCreditoActualmente'])+")")
+                    x['tipo'] ="ALTA TARJETA CREDITO"
+
+            elif x['tipo']== "ALTA_CHEQUERA":
+                if x['totalChequerasActualmente'] >=2:
+                    x.update(razon= "Actualmente tiene el maximo de chequeras ("+str(x['totalChequerasActualmente'])+")")
+                    x['tipo'] = "ALTA CHEQUERA"
+
+            elif x['tipo']== "COMPRA_DOLAR":
+                if x['monto']>x['saldoEnCuenta']:
+                    x.update(razon= "El monto de compra (" +str(x['monto']) + ") exede el saldo en cuenta (" +str(x['saldoEnCuenta'])+")")
+                    x['tipo'] = "COMPRA DOLAR"
+
+            elif x['tipo']== "TRANSFERENCIA_ENVIADA":
+                if x['monto']> x['saldoEnCuenta']:
+                    x.update(razon= "El monto de la transferencia exede el saldo en cuenta")
+                    x['tipo'] = "TRANSFERENCIA ENVIADA"
+       
+            if  x['estado']=="ACEPTADA":
+                if x['tipo'] == "RETIRO_EFECTIVO_CAJERO_AUTOMATICO":
+                    x['tipo'] ="RETIRO EFECTIVO CAJERO AUTOMATICO"
+                    x['razon'] =""                    
+                if x['tipo']== "TRANSFERENCIA_RECIBIDA":
+                    x['tipo'] ="TRANSFERENCIA RECIBIDA"
+            
+
+
+        
+        return self.evento
